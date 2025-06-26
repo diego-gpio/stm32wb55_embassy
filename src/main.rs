407052b7aa1846ed931899f8408fb3f1a18c99e7
diego-gpio <diego@barranco.io>
2025-06-26 17:47:10 -0400

    Switched hello world example to embassy. RTC still not working for stm32wb but should be fixed in next release.

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,32 @@
 #![no_std]
 #![no_main]
 
-use cortex_m as _;
-use cortex_m::delay::Delay;
-use cortex_m_rt::entry; // The runtime
-
-use defmt::{info, warn};
+use defmt::*;
 use defmt_rtt as _;
-use hal::{
-    self,
-    clocks::Clocks,
-    gpio::{Pin, PinMode, Port, Pull},
-    pac,
+use embassy_executor::Spawner;
+use embassy_futures::join::join;
+use embassy_stm32::{
+    exti::{AnyChannel, Channel, ExtiInput},
+    gpio::{AnyPin, Pin, Pull},
 };
-use panic_halt as _;
+use embassy_time::Timer;
+use panic_probe as _;
 
-// This marks the entrypoint of our application. The cortex_m_rt creates some
-// startup code before this, but we don't need to worry about this
-#[entry]
-fn main() -> ! {
-    // Set up CPU peripherals
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
     info!("Starting...");
-    let cp = cortex_m::Peripherals::take().unwrap();
-    // Set up microcontroller peripherals
-    let mut _dp = pac::Peripherals::take().unwrap();
-    warn!("I'm here");
-
-    let clock_cfg = Clocks::default();
-
-    // Write the clock configuration to the MCU. If you wish, you can modify `clocks` above
-    // in accordance with [its docs](https://docs.rs/stm32-hal2/0.2.0/stm32_hal2/clocks/index.html),
-    // and the `clock_cfg` example.
-    clock_cfg.setup().unwrap();
-
-    // Setup a delay, based on the Cortex-m systick.
-    info!("Configuring peripherals");
-    let mut delay = Delay::new(cp.SYST, clock_cfg.systick());
-    let mut led1 = Pin::new(Port::B, 5, PinMode::Output);
-    let mut led2 = Pin::new(Port::B, 0, PinMode::Output);
-    let mut led3 = Pin::new(Port::B, 1, PinMode::Output);
-    let mut sw1 = Pin::new(Port::C, 4, PinMode::Input);
-    let mut sw2 = Pin::new(Port::D, 0, PinMode::Input);
-    sw1.pull(Pull::Up);
-    sw2.pull(Pull::Up);
-    // let mut sw3 = Pin::new(Port::D, 1, PinMode::Input);
-
-    loop {
-        if sw1.is_low() {
-            info!("Turning ON LEDs");
-            led1.set_high();
-            led2.set_high();
-            led3.set_high();
-            delay.delay_ms(1_000);
-        }
-        if sw2.is_low() {
-            info!("Turning OFF LEDs");
-            led1.set_low();
-            led2.set_low();
-            led3.set_low();
-            delay.delay_ms(1_000);
-        }
-    }
+    let p = embassy_stm32::init(Default::default());
+    let sw1 = button(p.PC4.degrade(), p.EXTI4.degrade(), "1");
+    let sw3 = button(p.PD1.degrade(), p.EXTI1.degrade(), "1");
+    join(sw1, sw3).await;
 }
 
-/// Terminates the application and makes `probe-run` exit with exit-code = 0
-pub fn exit() -> ! {
+async fn button(pin: AnyPin, int: AnyChannel, id: &str) {
+    let mut button = ExtiInput::new(pin, int, Pull::Up);
     loop {
-        cortex_m::asm::bkpt();
+        button.wait_for_falling_edge().await;
+        info!("Button {} pressed!!", id);
+        Timer::after_millis(200).await;
+        button.wait_for_rising_edge().await;
     }
 }
