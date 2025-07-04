7d6fc94a4a820dcb8f533170ea378625e22a1d25
diego-gpio <diego@barranco.io>
2025-07-03 21:32:54 -0400

    Added RTC as embassy time.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 #![no_std]
 #![no_main]
+
+//use chrono::NaiveDate;
 use defmt::*;
 use defmt_rtt as _;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
-use embassy_stm32::low_power::{Executor, StopMode, stop_ready, stop_with_rtc};
+use embassy_stm32::low_power::{Executor, StopMode, stop_ready};
 use embassy_stm32::rtc::{Rtc, RtcConfig};
 use embassy_time::Timer;
 use panic_probe as _;
 use static_cell::StaticCell;
+
+mod led;
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
@@ -22,41 +25,39 @@ async fn async_main(spawner: Spawner) {
     // initialize the platform...
     let mut config = embassy_stm32::Config::default();
     // when enabled the power-consumption is much higher during stop, but debugging and RTT is working
-    config.enable_debug_during_sleep = false;
-    static CONFIG: StaticCell<embassy_stm32::Config> = StaticCell::new();
-    let config = CONFIG.init(config);
-    let p = embassy_stm32::init(*config);
+    config.enable_debug_during_sleep = true;
+    let p = embassy_stm32::init(config);
 
-    // give the RTC to the executor...
     let rtc = Rtc::new(p.RTC, RtcConfig::default());
     static RTC: StaticCell<Rtc> = StaticCell::new();
     let rtc = RTC.init(rtc);
-    stop_with_rtc(rtc);
+    embassy_stm32::low_power::stop_with_rtc(rtc);
 
-    spawner.spawn(led_toggle(*config)).unwrap();
-    spawner.spawn(task_2()).unwrap();
+    spawner.spawn(led::toggle_red(p.PB1.into())).unwrap();
+    spawner.spawn(led::toggle_green(p.PB0.into())).unwrap();
+    // spawner.spawn(task_1()).unwrap();
+    // spawner.spawn(task_2()).unwrap();
 }
 
 #[embassy_executor::task]
-async fn led_toggle(config: embassy_stm32::Config) {
-    let p = embassy_stm32::init(config);
-    let mut led = Output::new(p.PB1, Level::Low, Speed::Low);
-    for _ in 0..9 {
-        info!("Toggle LED 10 times");
-        led.toggle();
-        Timer::after_millis(100).await;
+async fn task_1() {
+    loop {
+        for _ in 0..9 {
+            if stop_ready(StopMode::Stop2) {
+                info!("task 1: waiting for 5000ms...");
+                Timer::after_millis(5000).await;
+            }
+        }
     }
-    // disable LED
-    led.set_low();
-    defmt::assert!(stop_ready(StopMode::Stop2));
 }
 
 #[embassy_executor::task]
 async fn task_2() {
-    for _ in 0..5 {
-        info!("task 2: waiting for 1000ms...");
-        defmt::assert!(stop_ready(StopMode::Stop2));
-        Timer::after_millis(1000).await;
+    for _ in 0..50 {
+        if stop_ready(StopMode::Stop2) {
+            info!("task 2: waiting for 10000ms...");
+            Timer::after_millis(60000).await;
+        }
     }
 
     info!("Test OK");
